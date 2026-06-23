4f4b43325b832f7a88efa70d59b0b296b908562e
lilconsev12-code <lilconsev12@gmail.com>
2026-06-23 14:47:16 +0000

    hi

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 import socket
 import threading
+import os
 
 HOST = "0.0.0.0"
-PORT = 5555
+PORT = int(os.environ.get("PORT", 5555))
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
@@ -11,28 +12,37 @@ server.listen()
 clients = []
 
 def broadcast(message):
-    for client in clients:
+    for client in clients[:]:
         try:
             client.send(message)
         except:
-            pass
+            clients.remove(client)
 
 def handle(client):
     while True:
         try:
             msg = client.recv(1024)
+            if not msg:
+                break
             broadcast(msg)
         except:
-            if client in clients:
-                clients.remove(client)
-            client.close()
             break
+
+    if client in clients:
+        clients.remove(client)
+
+    client.close()
 
 print(f"Server running on port {PORT}")
 
 while True:
     client, addr = server.accept()
+    print(f"Connected: {addr}")
+
     clients.append(client)
 
-    thread = threading.Thread(target=handle, args=(client,))
-    thread.start()
+    threading.Thread(
+        target=handle,
+        args=(client,),
+        daemon=True
+    ).start()
